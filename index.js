5d5208856fe7b3d31cdaabb4907f529eb403efe6
ShamimHossain1 <beastb697@gmail.com>
2023-05-04 02:27:53 +0600

    recipies data

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+
+const chef = require('./Data/chefs.json')
+
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send('Server is running')
+
+    res.send(chef)
 });
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    const details = chef.find(n => n.id === id)
+    res.send(details)
+
+})
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`)
+})
+
