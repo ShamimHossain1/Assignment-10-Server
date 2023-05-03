42ea306b41a622587059beb4c7fc53b8ccffe713
ShamimHossain1 <beastb697@gmail.com>
2023-05-03 16:04:49 +0600

    'setup'

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+});
